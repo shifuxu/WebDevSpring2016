13357e64149466c2e779fd99b913057ee17f5d6a
Shifu Xu <steven.shifu.xu@gmail.com>
2016-01-20 19:10:18 -0500

    simplified server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
+
+function rootRequest (req, res) {
+    res.send('hello world')
+}
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/hello', rootRequest);
 app.listen(port, ipaddress);
