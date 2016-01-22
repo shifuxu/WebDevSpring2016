226f61c66d1902d72f405528efca86f586919a56
Shifu Xu <steven.shifu.xu@gmail.com>
2016-01-22 16:17:41 -0500

    add bootstrap into assignmnet 1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3010;
 
 function rootRequest (req, res) {
     res.send('hello world')
