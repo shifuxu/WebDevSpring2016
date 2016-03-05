56c34db87afe1b67fcb9239cefc92668d1bc599d
Shifu Xu <steven.shifu.xu@gmail.com>
2016-03-05 16:33:07 -0500

    add field service in the server side

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -8,7 +8,12 @@ module.exports = function() {
         findAllForms: findAllForms,
         deleteFormById: deleteFormById,
         updateFormById: updateFormById,
-        findFormByTitle: findFormByTitle
+        findFormByTitle: findFormByTitle,
+        findAllFieldsById: findAllFieldsById,
+        findFieldByFieldIdAndFormId: findFieldByFieldIdAndFormId,
+        deleteFieldByFieldIdAndFormId: deleteFieldByFieldIdAndFormId,
+        createFieldForForm: createFieldForForm,
+        updateFieldForForm: updateFieldForForm
     };
 
     return api;
@@ -79,4 +84,76 @@ module.exports = function() {
 
         return null;
     }
+
+    function findAllFieldsById(formId) {
+        var form = findFormById(formId);
+        if (!form) {
+            return form.fields;
+        }
+
+        return [];
+    }
+
+    function findFieldByFieldIdAndFormId(formId, fieldId) {
+        var fields = findAllFieldsById(formId);
+        if (!fields) {
+            for (var f in fields) {
+                if (fields[f] == fieldId) {
+                    return fields[f];
+                }
+            }
+        }
+
+        return null;
+    }
+
+    function deleteFieldByFieldIdAndFormId(formId, fieldId) {
+        var field = findFieldByFieldIdAndFormId(formId, fieldId);
+        var fields = findAllFieldsById(formId);
+        if (!field) {
+            fields.splice(fields.indexOf(filed), 1);
+        }
+        return null;
+    }
+
+    function createFieldForForm(formId, field) {
+        var newField = {
+            _id: (new Date).getTime(),
+            label: field.label,
+            type: field.type,
+            placeholder: field.placeholder,
+            options: field.options
+        };
+
+        var fields = findAllFieldsById(formId);
+        fields.push(newField);
+
+        // can we remove here?
+        var form = findFormById(formId);
+        var newForm = {
+            _id: form._id,
+            title: form.title,
+            userId: form.userId,
+            fields: fields
+        };
+
+        updateFormById(formId, newForm);
+
+        return newField;
+    }
+
+    function updateFieldForForm(formId, fieldId, field) {
+        var fieldTemp = findFieldByFieldIdAndFormId(formId, fieldId);
+        if (!fieldTemp) {
+            fieldTemp._id = field._id;
+            fieldTemp.label = field.label;
+            fieldTemp.type = field.type;
+            fieldTemp.placeholder = field.placeholder;
+            fieldTemp.options = field.options;
+            return fieldTemp;
+        } else {
+            return null;
+        }
+    }
+
 };
--- public/assignment/server/services/field.service.server.js
+++ public/assignment/server/services/field.service.server.js
@@ -1,7 +1,43 @@
 module.exports = function (app, userModel, formModel) {
-    app.get("/api/assignment/form/:formId/field");
-    app.get("/api/assignment/form/::formId/field/:fieldId");
-    app.delete("/api/assignment/form/:formId/field/:fieldId");
-    app.post("/api/assignment/form/:formId/field");
+    app.get("/api/assignment/form/:formId/field", findAllFieldsById);
+    app.get("/api/assignment/form/::formId/field/:fieldId", findFieldByFieldIdAndFormId);
+    app.delete("/api/assignment/form/:formId/field/:fieldId", deleteFieldByFieldIdAndFormId);
+    app.post("/api/assignment/form/:formId/field", createFieldForForm);
     app.put("/api/assignment/form/:formId/field/:fieldId");
+
+    function findAllFieldsById(req, res) {
+        var formId = req.params.formId;
+        var fields = formModel.findAllFieldsById(formId);
+        res.json(fields);
+    }
+
+    function findFieldByFieldIdAndFormId(req, res) {
+        var formId = req.params.formId;
+        var fieldId = req.params.fieldId;
+        var field = formModel.findFieldByFieldIdAndFormId(formId, fieldId);
+        res.json(field);
+    }
+
+    function deleteFieldByFieldIdAndFormId(req, res) {
+        var formId = req.params.formId;
+        var fieldId = req.params.fieldId;
+        var field = formModel.deleteFieldByFieldIdAndFormId(formId, fieldId);
+        res.json(formModel.findAllFieldsById(formId));
+    }
+
+    function createFieldForForm(req, res) {
+        var formId = req.params.formId;
+        var field = req.body;
+        var newField = formModel.createFieldForForm(formId, field);
+        res.json(formModel.findAllFieldsById(formId));
+    }
+
+    function updateFieldForForm(req, res) {
+        var formId = req.params.formId;
+        var fieldId = req.params.fieldId;
+        var field = req.body;
+        var newField = formModel.updateFieldForForm(formId, fieldId, field);
+        res.json(formModel.findAllFieldsById(formId));
+    }
+
 };
