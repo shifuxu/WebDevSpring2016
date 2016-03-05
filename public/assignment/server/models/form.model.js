b5eaf3a3bd448e02ae11dfb63c2cccb4b72d4e61
Shifu Xu <steven.shifu.xu@gmail.com>
2016-03-04 21:08:58 -0500

    add model for form and user in server side

---
+++ public/assignment/server/models/form.model.js
@@ -0,0 +1,81 @@
+var forms = require("./form.mock.json");
+
+module.exports = function() {
+    var api = {
+        createFormForUser: createFormForUser,
+        findAllFormsForUser: findAllFormsForUser,
+        findFormById: findFormById,
+        findAllForms: findAllForms,
+        deleteFormById: deleteFormById,
+        updateFormById: updateFormById,
+        findFormByTitle: findFormByTitle
+    };
+
+    return api;
+
+    function createFormForUser(userId, form) {
+        var newForm = {
+            _id: (new Date).getTime(),
+            title: form.title,
+            userId: userId
+        };
+
+        forms.push(newForm);
+        return newForm;
+    }
+
+    function findAllFormsForUser(userId) {
+        var res = [];
+        for (var f in forms) {
+            if (forms[f].userId == userId) {
+                res.push(forms[f]);
+            }
+        }
+
+        return res;
+    }
+
+    function findFormById(formId) {
+        for (var f in forms) {
+            if (forms[f]._id == formId) {
+                return forms[f];
+            }
+        }
+
+        return null;
+    }
+
+    function findAllForms() {
+        return forms;
+    }
+
+    function deleteFormById(formId) {
+        var form = findFormById(formId);
+        if (form != null) {
+            forms.splice(forms.indexOf(form), 1);
+        } else {
+            return null;
+        }
+    }
+
+    function updateFormById(formId, newForm) {
+        var formTemp = findFormById(formId);
+        if (formTemp != null) {
+            formTemp.title = newForm.title;
+            formTemp.userId = newForm.userId;
+            return formTemp;
+        } else {
+            return null;
+        }
+    }
+
+    function findFormByTitle(title) {
+        for (var f in forms) {
+            if (forms[f].title == title) {
+                return forms[f];
+            }
+        }
+
+        return null;
+    }
+};
---
+++ public/assignment/server/models/user.model.js
@@ -0,0 +1,91 @@
+var users = require("./user.mock.json");
+
+module.exports = function () {
+    var api = {
+        findUserByCredentials: findUserByCredentials,
+        findUserById: findUserById,
+        findUserByUsername: findUserByUsername,
+        findAllUsers: findAllUsers,
+        createUser: createUser,
+        deleteUserById: deleteUserById,
+        updateUser: updateUser
+    };
+
+    return api;
+
+    function findUserByCredentials(credentials) {
+        var username = credentials.username;
+        var password = credentials.password;
+
+        for (var u in users) {
+            if (users[u].username == username && users[u].password == password) {
+                return users[u];
+            }
+        }
+
+        return null;
+    }
+
+    function findUserById(userId) {
+        for (var u in users){
+            if (users[u]._id == userId) {
+                return users[u];
+            }
+        }
+
+        return null;
+    }
+
+    function findUserByUsername(username) {
+        for (var u in users){
+            if (users[u].username == username) {
+                return users[u];
+            }
+        }
+
+        return null;
+    }
+
+    function findAllUsers() {
+        return users;
+    }
+
+    function createUser(user) {
+        var newUser = {
+            _id: (new Date).getTime(),
+            username: user.username,
+            password: user.password,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            roles: user.roles,
+            email: user.email
+        };
+
+        users.push(newUser);
+        return newUser;
+    }
+
+    function deleteUserById(userId) {
+        var user = findUserById(userId);
+        if (user != null) {
+            users.splice(users.indexOf(user), 1);
+        } else {
+            return null;
+        }
+    }
+
+    function updateUser(userId, user) {
+        var userTemp = findUserById(userId);
+        if (userTemp != null) {
+            userTemp.firstName = user.firstName;
+            userTemp.lastName = user.lastName;
+            userTemp.password = user.password;
+            userTemp.username = user.username;
+            userTemp.roles = user.roles;
+            userTemp.email = user.email;
+            return userTemp;
+        } else {
+            return null;
+        }
+    }
+};
