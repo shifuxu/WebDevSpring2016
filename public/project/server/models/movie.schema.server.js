767370b7286ab2d38c7926daebd4ede62c3e369f
Shifu Xu <steven.shifu.xu@gmail.com>
2016-03-26 18:57:30 -0400

    add two schemas

--- public/project/server/models/movie.schema.server.js
+++ public/project/server/models/movie.schema.server.js
@@ -1,1 +1,25 @@
+module.exports = function(mongoose) {
 
+    // use mongoose to declare a movie schema
+    var MovieSchema = mongoose.Schema({
+        imdbID: String,
+        title: String,
+        poster: String,
+        // ids of users that like this movie
+        likes: [String],
+        // list of users that like this movie
+        userLikes: [
+            {username: String}
+        ],
+        // list of users that has reviewed this movie
+        userReviews: [
+            {
+                username: String,
+                comments: String
+            }
+        ]
+    }, {collection: 'project.omdb.movie'});
+
+    return MovieSchema;
+
+};
